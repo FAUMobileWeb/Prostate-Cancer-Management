1867cb0b4471702a5cceea8e67692028705bbfab
Ericky14 <edossantos2014@fau.edu>
2016-10-27 22:46:14 -0400

    Turned homework 2 into a .rar file

--- Homeworks/Homework2/public_html/php/php_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db = mysqli_connect("localhost", "db_username", "db_password", "db_name");
-
-    if (mysqli_connect_error()) {
-        echo mysqli_connect_error();
-        exit();
-    }
-?>
--- Homeworks/Homework2/public_html/php/redirect-if-logged.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    if (isset($_SESSION['username'])) {
-        header("location: ../prostate-cancer.html#dashboard");
-        exit();
-    }
-?>
